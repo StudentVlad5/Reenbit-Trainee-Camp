99ef9e2e9f56ad8dd9b2d30ad9956bdf8c3b97f6
Vlad <vlad_np@ukr.net>
2024-12-05 16:22:59 +0100

    change BaseUrl

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3030/api";
+const BASE_URL = "https://reenbit-trainee-camp.vercel.app/";
+// const BASE_URL = "http://localhost:3030/api";
 
 export const signUp = async (credentials) => {
   try {
